a87a9e1d8c99aa991be68583f1f94eba0e17b67c
Andrew Coffey <andrewryancoffey@hotmail.com>
2023-10-27 18:13:45 -0500

    added GameState abstract class and GameStateManager, also set up shared

---
+++ src/gamestate.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+import shared
+
+
+class GameState(ABC):
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    @abstractmethod
+    def handle_events(self) -> None:
+        pass
+
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def draw(self) -> None:
+        pass
+
+
+class GameStateManager:
+    __instance = None
+    __initialized = False
+
+    def __new__(cls) -> GameStateManager:
+        if cls.__instance is None:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
+
+    def __init__(self) -> None:
+        if not GameStateManager.__initialized:
+            self.states: dict[str, GameState] = {}
+            self.state: GameState | None = None
+            GameStateManager.__initialized = True
+
+    def add_state(self, state: GameState) -> None:
+        self.states[state.name] = state
+
+    def set_state(self, name: str) -> None:
+        self.state = self.states[name]
+
+    def update(self) -> None:
+        if self.state is not None:
+            self.state.update()
+
+    def draw(self) -> None:
+        if self.state is not None:
+            self.state.draw()
+
+    def handle_events(self) -> None:
+        if self.state is not None:
+            self.state.handle_events(shared.events)
---
+++ src/shared.py
@@ -0,0 +1,8 @@
+import pygame
+
+screen: pygame.Surface
+clock: pygame.Clock
+events: list[pygame.event.Event]
+keys: list[bool]
+dt: float
+mouse_pos: pygame.Vector2
