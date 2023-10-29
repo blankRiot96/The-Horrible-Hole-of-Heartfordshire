a1c732996f09c3d23b967d9b30e2697d0836cced
Axis <77634274+blankRiot96@users.noreply.github.com>
2023-10-29 21:52:25 +0530

    added comment in enums.py for clarification

--- src/enums.py
+++ src/enums.py
@@ -9,6 +9,9 @@ class MovementType(Enum):
 
 
 class DoorDirection(Enum):
+    # The values indicate the direction in which the player needs to move towards
+    # if entering from the respective NEWS door
+    # So if you enter from east gate, you want to move towards the left/west
     NORTH = (0, 1)
     EAST = (-1, 0)
     WEST = (1, 0)
