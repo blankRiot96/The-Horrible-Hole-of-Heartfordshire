93313c04f744d67d0f052c83ab6fbe31dba8c896
Axis <77634274+blankRiot96@users.noreply.github.com>
2023-10-29 20:47:22 +0530

    switched center tiles to wall tiles

--- assets/data/tmx/tiles.tsx
+++ assets/data/tmx/tiles.tsx
@@ -1,5 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="64" tileheight="64" tilecount="36" columns="6">
  <image source="../../art/tileset-64.png" width="384" height="384"/>
+ <tile id="0" type="wall"/>
+ <tile id="1" type="wall"/>
+ <tile id="2" type="wall"/>
+ <tile id="3" type="wall"/>
+ <tile id="4" type="wall"/>
+ <tile id="6" type="wall"/>
+ <tile id="7" type="wall"/>
+ <tile id="8" type="wall"/>
+ <tile id="9" type="wall"/>
+ <tile id="10" type="wall"/>
+ <tile id="12" type="wall"/>
+ <tile id="13" type="wall"/>
+ <tile id="15" type="wall"/>
+ <tile id="16" type="wall"/>
+ <tile id="18" type="wall"/>
+ <tile id="19" type="wall"/>
+ <tile id="20" type="wall"/>
+ <tile id="21" type="wall"/>
+ <tile id="22" type="wall"/>
+ <tile id="24" type="wall"/>
+ <tile id="25" type="wall"/>
+ <tile id="26" type="wall"/>
+ <tile id="27" type="wall"/>
+ <tile id="28" type="wall"/>
  <tile id="30" type="torch"/>
 </tileset>
