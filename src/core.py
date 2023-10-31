f14456360ac30b9eacc178a35933b6718dd7a9e1
Andrew Coffey <andrewryancoffey@hotmail.com>
2023-10-30 21:44:23 -0500

    added intro

--- src/core.py
+++ src/core.py
@@ -13,10 +13,12 @@ class Core:
         shared.dt = 0.0
         shared.events = []
 
+        from .introstate import IntroState
         from .playstate import PlayState
 
         GameStateManager().add_state(PlayState)
-        GameStateManager().set_state("PlayState")
+        GameStateManager().add_state(IntroState)
+        GameStateManager().set_state("IntroState")
 
     def win_init(self) -> None:
         pygame.init()
